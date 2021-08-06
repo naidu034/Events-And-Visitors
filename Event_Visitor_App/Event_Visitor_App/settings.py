fc9d32d4668faa253887c5552a6e491d29561f0a
Amareswara naidu Kamma <64314176+naidu034@users.noreply.github.com>
2021-08-06 14:01:30 +0530

    Update settings.py

--- Event_Visitor_App/Event_Visitor_App/settings.py
+++ Event_Visitor_App/Event_Visitor_App/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=uhyfa3hvqem7ioshh9wmzqb!gyy$*^8mi8j1=u7p^c4p*ft)3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
