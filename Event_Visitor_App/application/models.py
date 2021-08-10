6c4fd70c2be3a61c3c042388c75e9f7e3ca3f313
Naidu <naidu.19bci7002@vitap.ac.in>
2021-08-10 17:51:40 +0530

    superuser,visitor model created

--- Event_Visitor_App/application/models.py
+++ Event_Visitor_App/application/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+
+
+class Visitor(models.Model):
+    datetime = models.DateTimeField(default=now, editable=False)
+    visitorname = models.CharField(max_length=50)
+    tomeet = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    address = models.TextField()
+    purpose = models.TextField()
+    mobile = models.IntegerField(max_length=10)
+
+    class Meta:
+        db_table = "Visitor"
+
+    def __str__(self):
+        return self.visitorname
+
+
