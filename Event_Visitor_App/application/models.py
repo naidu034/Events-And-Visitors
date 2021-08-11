eb750e0b934c3936a2bd5075968b1e527f45eb9e
Naidu <naidu.19bci7002@vitap.ac.in>
2021-08-11 17:17:02 +0530

    models created

--- Event_Visitor_App/application/models.py
+++ Event_Visitor_App/application/models.py
@@ -11,12 +11,29 @@ class Visitor(models.Model):
     department = models.CharField(max_length=50)
     address = models.TextField()
     purpose = models.TextField()
-    mobile = models.IntegerField(max_length=10)
+    mobile = models.IntegerField()
 
     class Meta:
         db_table = "Visitor"
 
     def __str__(self):
         return self.visitorname
+
+class Event(models.Model):
+    datetime = models.DateTimeField(default=now, editable=False)
+    organsier = models.CharField(max_length=50)
+    attendees = models.TextField()
+    department = models.CharField(max_length=50)
+    summary = models.TextField()
+    purpose = models.TextField()
+    feedback = models.TextField()
+
+    class Meta:
+        db_table = "Event"
+
+    def __str__(self):
+        return self.organsier
+
+
 
 
